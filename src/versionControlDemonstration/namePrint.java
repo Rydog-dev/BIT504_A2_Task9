bdb08cd338dff98ab4287eb0a906fc9a6eb102b8
Ryan <rwmccormick1806@gmail.com>
2026-04-16 16:25:25 +1200

    Initial commit - added main class

---
+++ src/versionControlDemonstration/namePrint.java
@@ -0,0 +1,12 @@
+package versionControlDemonstration;
+
+public class namePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("BIT504 Assessment 2 Task 9");
+		System.out.println("Ryan McCormick");
+
+	}
+
+}
